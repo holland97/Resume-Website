35cc319d719f5941d21cf0a531f93c1ebed1e419
holland97 <103268325+holland97@users.noreply.github.com>
2023-10-13 13:15:11 -0400

    terraform/provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,1 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      version = "~> 5.2"
+      source  = "hashicorp/aws"
+    }
+  }
+}
 
+provider "aws" {
+  region  = "us-east-1"
+  profile = "AdminViper-"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-bkt"
+    key    = ""
+    region = "us-east-1"
+  }
+}
